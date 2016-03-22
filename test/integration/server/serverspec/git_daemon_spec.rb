40cfd1903e40a44f3a38ae85fc4cde20429256cd
Dheeraj Karande <dheeraj.karande@weboniselab.com>
2016-03-22 16:11:39 +0530

    changes in server test

--- test/integration/server/serverspec/git_daemon_spec.rb
+++ test/integration/server/serverspec/git_daemon_spec.rb
@@ -3,20 +3,20 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe "Git Daemon" do
+#describe "Git Daemon" do
 
-  it "is listening on port 9418" do
-    expect(port(22)).to be_listening
-  end
+#  it "is listening on port 9418" do
+#    expect(port(22)).to be_listening
+#  end
 
-  it "has a running service of git-daemon" do
-    expect(service("sshd")).to be_running
-  end
+#  it "has a running service of git-daemon" do
+#    expect(service("sshd")).to be_running
+#  end
 
-describe port(22) do
-  it { should be_listening }
-end
+#describe port(22) do
+#  it { should be_listening }
+#end
 describe user('root') do
   it { should have_home_directory '/root' }
 end
-end
+#end
